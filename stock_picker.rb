d40f80e3c09ed59a32c0aff84a4fcc392d0ae7f4
Michael Daryl Mayo <michaeldaryl.code@gmail.com>
2017-08-09 20:57:06 +0800

    Adjust number of lines of code

--- stock_picker.rb
+++ stock_picker.rb
@@ -2,8 +2,6 @@ stock_prices = [17,3,6,9,15,8,6,1,10]
 highest_combi_of_days = [0, 0, 0]
 
 stock_prices.each_with_index do |value, index|
-
-
   for j in index+1...stock_prices.length
       profit = stock_prices[j] - stock_prices[index];
 
