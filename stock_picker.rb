6190166fe9a2ce916d3ba71a7658cf1d8ec04a2d
Michael Daryl Mayo <michaeldaryl.code@gmail.com>
2017-08-09 21:09:40 +0800

    Refactor stock_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,18 +1,19 @@
-stock_prices = [17,3,6,9,15,8,6,1,10]
-highest_combi_of_days = [0, 0, 0]
+def stockprice(stock_prices)
+  max = [0, 0, 0] # profit, buy_day, sell_day
 
-stock_prices.each_with_index do |value, index|
-  for j in index+1...stock_prices.length
-      profit = stock_prices[j] - stock_prices[index];
+  stock_prices.each_with_index do |value, index|
+    for j in index + 1...stock_prices.length
+      profit = stock_prices[j] - stock_prices[index]
 
-      if profit > highest_combi_of_days[0]
-        highest_combi_of_days[0] = profit
-        highest_combi_of_days[1] = index
-        highest_combi_of_days[2] = j
+      if profit > max[0]
+        max[0] = profit
+        max[1] = index
+        max[2] = j
       end
+    end
   end
 
+  puts "profit: #{max[0]}, buy: #{max[1]} sell: #{max[2]}"
 end
 
-print "buy: #{highest_combi_of_days[1]} sell: #{highest_combi_of_days[2]} "
-print "profit: #{highest_combi_of_days[0]}"
+stockprice([17,3,6,9,15,8,6,1,10])
